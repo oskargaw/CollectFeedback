86bdce81cbca9ac29b2d8b28e68643458daf3e95
Oskar Gawlak <oskar.gawlak@gmail.com>
2017-10-01 23:45:45 +0200

    Added billing and client side app

--- index.js
+++ index.js
@@ -11,11 +11,9 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-// Express' middlewares
 app.use(bodyParser.json());
 app.use(
   cookieSession({
-    // 30 days expressed in miliseconds
     maxAge: 30 * 24 * 60 * 60 * 1000,
     keys: [keys.cookieKey]
   })
@@ -28,8 +26,7 @@ require('./routes/billingRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
   // Express will serve up production assets
-  // like our main.js file or main.css file
-  // if it doesn't recognize the request
+  // like our main.js file, or main.css file!
   app.use(express.static('client/build'));
 
   // Express will serve up the index.html file
